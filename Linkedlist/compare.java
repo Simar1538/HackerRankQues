d35abedfb02bb6acfc15b24e2854d1986d139119
Simar1538 <76070677+Simar1538@users.noreply.github.com>
2021-03-14 02:37:07 +0530

    Compare two linked lists

---
+++ Linkedlist/compare.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the compareLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode trav1=head1,trav2=head2;
+        while(trav1.next!=null&&trav2.next!=null)
+        {
+              if(trav1.data==trav2.data)
+              {
+                  trav1=trav1.next;
+                  trav2=trav2.next;
+              }
+              else
+                return false;
+        }
+       if(trav1.next==null&&trav2.next==null)
+         return true;
+
+       else
+         return false;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
