af823097d10a891d1920743812226c178d5778cb
Simar1538 <76070677+Simar1538@users.noreply.github.com>
2021-03-17 01:38:09 +0530

    Inserting a Node Into a Sorted Doubly Linked List

---
+++ Linkedlist/insort.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the sortedInsert function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode head, int data) {
+        
+                DoublyLinkedListNode trav=head;
+                DoublyLinkedListNode node = new DoublyLinkedListNode(data);
+            if(head.data>data)
+            {
+                node.next=head;
+                head.prev=node;
+                head=node;
+                node.prev=null;
+            }
+            else
+            {
+                while(trav.next!=null&&trav.next.data<data)
+                {
+                    trav=trav.next;
+                }
+                node.next=trav.next;
+                trav.next=node;
+                node.prev=trav;
+            }
+          return head;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
